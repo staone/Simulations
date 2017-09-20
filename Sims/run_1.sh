be8b3d03c2854fcdf77e59f2aa17984306592b14
staone <stavan.p.shah@gmail.com>
2017-09-21 00:29:25 +0530

    some changes

--- Sims/run_1.sh
+++ Sims/run_1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-g++ sim3_0.cpp -o a2
+g++ sim2_2.cpp -o a2
 for file in ./*.txt
 do
 ./a2 < $file >> output1.txt
--- Sims/run_2.sh
+++ Sims/run_2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-g++ sim2_2.cpp -o a1
+g++ sim3_0.cpp -o a1
 for file in ./*.txt
 do
 ./a1 < $file >> output2.txt
